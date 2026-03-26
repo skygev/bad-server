714fc1466cd77cf97ad65be670e9ac5d4666a323
Elena Gorozheeva <prodatamax@yandex.ru>
2026-03-26 15:17:30 +0300

    Restrict admin-only routes

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
+import auth, { roleGuardMiddleware } from '../middlewares/auth'
 import fileMiddleware from '../middlewares/file'
+import { Role } from '../models/user'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+uploadRouter.post(
+    '/',
+    auth,
+    roleGuardMiddleware(Role.Admin),
+    fileMiddleware.single('file'),
+    uploadFile
+)
 
 export default uploadRouter
